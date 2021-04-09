52662499bc74e4eb1cd0e3cbf8cdfe9518d0a4eb
johannb75 <67163473+johannb75@users.noreply.github.com>
2021-04-09 13:53:13 +0200

    Added function and jest tests with example mjml file

--- src/__tests__/index.jest.ts
+++ src/__tests__/index.jest.ts
@@ -1,3 +1,13 @@
+import { mjml2HTML } from "../index";
+
 test("should return the file with variables changed", () => {
-	expect("jest works").toEqual("jest works");
+	expect(
+		mjml2HTML(
+			__dirname + "/test.mjml",
+			{
+				name: "hi",
+			},
+			{ validationLevel: "skip" }
+		)
+	).resolves.toContain("hi");
 });
---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import fs from "fs";
+import path from "path";
+import mjml2html from "mjml";
+import { MJMLParsingOptions } from "mjml-core";
+
+interface IVariables {
+	[variable: string]: string;
+}
+
+type TMJML2HTML = (
+	mjml_path: string,
+	vars?: IVariables,
+	options?: MJMLParsingOptions
+) => Promise<string>;
+
+/**
+ * Convert mjml file to html and replace variables by values
+ * @param mjml_path - The path to the mjml file
+ * @param vars - The variables to pass to the template
+ * @param options - The options for mjml parsing
+ * @returns The HTML of the file with variables replaced by values
+ */
+export const mjml2HTML: TMJML2HTML = async (
+	mjml_path,
+	vars = {},
+	options = {}
+) => {
+	try {
+		let mjml_code: string = (
+			await fs.promises.readFile(path.join(mjml_path), "utf8")
+		).trim();
+
+		for (let name of Object.keys(vars)) {
+			const value = vars[name];
+			mjml_code = mjml_code.replace(
+				new RegExp(`{{ \\$${name} }}`, "gm"),
+				value
+			);
+		}
+
+		const html_code = mjml2html(mjml_code, options);
+
+		if (html_code.errors[0]) throw html_code.errors?.[0]?.formattedMessage;
+
+		return html_code.html;
+	} catch (e) {
+		throw new Error(e);
+	}
+};
